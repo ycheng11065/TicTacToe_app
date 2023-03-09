05eeee9cc67997bc41ec7fae20315b2b784cb58b
Yu Cheng Li <ychengli11065@gmail.com>
2023-03-09 01:09:01 -0800

    Finished the tictactoe grid

---
+++ script.js
@@ -0,0 +1,18 @@
+const gameBoard = (() => {
+  let gameboard = [];
+  //  const players = [];
+
+  const newGame = () => {
+    gameboard = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+  };
+
+//   const addPlayers = (newPlayer) => {
+//     players.push(newPlayer);
+//   };
+})();
+
+// const player = (name) => ({ name });
