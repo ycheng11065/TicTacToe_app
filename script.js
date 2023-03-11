b94e4df8f8d2cd0e2dd319be5991b264f2875129
Yu Cheng Li <ychengli11065@gmail.com>
2023-03-10 22:53:27 -0800

    js kinda broken, css looks pretty good

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-const gameBoard = (() => {
-  let gameboard = [];
-  //  const players = [];
+const player = (symbol) => ({symbol});
 
-  const newGame = () => {
-    gameboard = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
-    ];
+const gameBoard = (() => {
+  // let gameboard = [];
+  const players = [];
+  let me;
+
+  const newGame = (symbol) => {
+    // gameboard = [
+    //   [0, 0, 0],
+    //   [0, 0, 0],
+    //   [0, 0, 0],
+    // ];
+
+    players.push(player(symbol));
+    [me] = players;
   };
 
-//   const addPlayers = (newPlayer) => {
-//     players.push(newPlayer);
-//   };
+  const render = (box) => {
+    box.innerHTML = `
+        <div class="flex-center-o" style="display: none">
+          <div class="o"></div>
+        </div>
+        <div class="x" style="display: none"></div>
+    `;
+
+  }
 })();
 
-// const player = (name) => ({ name });
+const game = gameBoard();
+game.newGame("x");
+
+const gameBoxes = document.querySelectorAll(".box");
+
+gameBoxes.forEach((gameBox) => {
+  gameBox.addEventListener("click", () => {
+    const { children } = gameBox;
+    const styles1 = getComputedStyle(children[0]);
+    const styles2 = getComputedStyle(children[1]);
+    console.log(styles1.display);
+    console.log(styles2.display);
+
+    if (styles1.display === "none" && styles2.display === "none") {
+      game.render(gameBox);
+    }
+  });
+});
