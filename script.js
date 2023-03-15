b32f37dd0c428eeb7dc66ac70819548b4d0c6690
Yu Cheng Li <ychengli11065@gmail.com>
2023-03-14 21:16:41 -0700

    added minimax ai

--- script.js
+++ script.js
@@ -32,39 +32,73 @@ const ticTacToe = (() => {
         availableMoves.push(address);
       }
     }
+    // console.log(currentPlayer);
+    aiMoveHard();
   };
 
   const hasWon = () => {
+    let winner = null;
+    let emptyBoxes = 0;
+
     // Check horizontal
     for (let i = 0; i < gameBoard.length; i += 1) {
       if (gameBoard[i][0] !== 0 && gameBoard[i][0] === gameBoard[i][1]
         && gameBoard[i][1] === gameBoard[i][2]) {
-        console.log("horizontal");
-        return true;
+        // console.log("horizontal");
+        if (gameBoard[i][0] === 1) {
+          winner = "x";
+        } else {
+          winner = "o";
+        }
       }
     }
     // Check vertical
     for (let i = 0; i < gameBoard[0].length; i += 1) {
       if (gameBoard[0][i] !== 0 && gameBoard[0][i] === gameBoard[1][i]
         && gameBoard[1][i] === gameBoard[2][i]) {
-        console.log("vertical");
-        return true;
+        // console.log("vertical");
+        if (gameBoard[0][i] === 1) {
+          winner = "x";
+        } else {
+          winner = "o";
+        }
       }
     }
 
     // Check diagonal
     if (gameBoard[0][0] !== 0 && gameBoard[0][0] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[2][2]) {
-      console.log("diagonal");
-      return true;
+      // console.log("diagonal");
+      if (gameBoard[0][0] === 1) {
+        winner = "x";
+      } else {
+        winner = "o";
+      }
     }
 
     if (gameBoard[0][2] !== 0 && gameBoard[0][2] === gameBoard[1][1]
       && gameBoard[1][1] === gameBoard[2][0]) {
-      console.log("diagonal");
-      return true;
+      // console.log("diagonal");
+      if (gameBoard[0][2] === 1) {
+        winner = "x";
+      } else {
+        winner = "o";
+      }
     }
-    return false;
+
+    // check if draw
+    for (let i = 0; i < gameBoard.length; i += 1) {
+      for (let j = 0; j < gameBoard[0].length; j += 1) {
+        if (gameBoard[i][j] === 0) {
+          emptyBoxes += 1;
+        }
+      }
+    }
+
+    if (winner === null && emptyBoxes === 0) {
+      return "draw";
+    }
+    return winner;
   };
 
   const removeMove = (x, y) => {
@@ -124,52 +158,144 @@ const ticTacToe = (() => {
     turn += 1;
   };
 
-  const aiMove = () => {
-    const mainIcon = players[0].symbol;
-    const min = 0;
-    const max = availableMoves.length - 1;
-    const randomVal = Math.floor(Math.random() * (max - min + 1)) + min;
-    const currAddress = availableMoves[randomVal];
-    const xPos = currAddress.charAt(1);
-    const yPos = currAddress.charAt(2);
-    const currBox = document.getElementById(currAddress);
+  // const aiMoveEasy = () => {
+  //   const mainIcon = players[0].symbol;
+  //   const min = 0;
+  //   const max = availableMoves.length - 1;
+  //   const randomVal = Math.floor(Math.random() * (max - min + 1)) + min;
+  //   const currAddress = availableMoves[randomVal];
+  //   const xPos = currAddress.charAt(1);
+  //   const yPos = currAddress.charAt(2);
+  //   const currBox = document.getElementById(currAddress);
 
-    availableMoves.splice(randomVal, 1);
+  //   availableMoves.splice(randomVal, 1);
 
-    if (mainIcon === "x") {
-      currBox.innerHTML = `
-          <div class="flex-center-o">
-            <div class="o"></div>
-          </div>
-          <div class="x" style="display: none"></div>
-        `;
-      gameBoard[xPos][yPos] = 2;
-    } else {
-      currBox.innerHTML = `
-        <div class="flex-center-o" style="display: none">
-          <div class="o"></div>
-        </div>
-        <div class="x"></div>
-        `;
-      gameBoard[xPos][yPos] = 1;
+  //   if (mainIcon === "x") {
+  //     currBox.innerHTML = `
+  //         <div class="flex-center-o">
+  //           <div class="o"></div>
+  //         </div>
+  //         <div class="x" style="display: none"></div>
+  //       `;
+  //     gameBoard[xPos][yPos] = 2;
+  //   } else {
+  //     currBox.innerHTML = `
+  //       <div class="flex-center-o" style="display: none">
+  //         <div class="o"></div>
+  //       </div>
+  //       <div class="x"></div>
+  //       `;
+  //     gameBoard[xPos][yPos] = 1;
+  //   }
+  // };
+
+  const miniMax = (board, depth, isMaximizing) => {
+    const winner = hasWon();
+    if (winner !== null) {
+      // console.log(winner);
+      if (winner === "o") {
+        return -10;
+      }
+      if (winner === "x") {
+        return 10;
+      }
+      if (winner === "draw") {
+        return 0;
+      }
     }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i += 1) {
+        for (let j = 0; j < 3; j += 1) {
+          if (gameBoard[i][j] === 0) {
+            gameBoard[i][j] = 1;
+            const score = miniMax(board, depth + 1, false);
+            gameBoard[i][j] = 0;
+            bestScore = Math.max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i += 1) {
+      for (let j = 0; j < 3; j += 1) {
+        if (gameBoard[i][j] === 0) {
+          gameBoard[i][j] = 2;
+          const score = miniMax(board, depth + 1, true);
+          gameBoard[i][j] = 0;
+          bestScore = Math.min(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  };
+
+  const aiMove = () => {
+    let bestScore = -Infinity;
+    let bestMove;
+    for (let i = 0; i < 3; i += 1) {
+      for (let j = 0; j < 3; j += 1) {
+        if (gameBoard[i][j] === 0) {
+          gameBoard[i][j] = 1;
+          const score = miniMax(gameBoard, 0, false);
+          gameBoard[i][j] = 0;
+          if (score > bestScore) {
+            bestScore = score;
+            bestMove = { i, j };
+          }
+        }
+      }
+    }
+    const xPos = bestMove.i;
+    const yPos = bestMove.j;
+    gameBoard[xPos][yPos] = 1;
+    // const currBox = document.getElementById(`p${xPos}${yPos}`);
+    // currBox.innerHTML = `
+    //   <div class="flex-center-o">
+    //     <div class="o"></div>
+    //   </div>
+    //   <div class="x" style="display: none"></div>
+    // `;
+    // const current = document.querySelector(`#p${xPos}${yPos} .flex-center-o`);
+    const currBox = document.getElementById(`p${xPos}${yPos}`);
+    currBox.innerHTML = `
+      <div class="flex-center-o" style="display: none">
+        <div class="o"></div>
+      </div>
+      <div class="x" ></div>
+    `;
+    const current = document.querySelector(`#p${xPos}${yPos} .x`);
+    current.style.animationDelay = ".3s";
+  };
+
+  const aiMoveHard = () => {
+    aiMove();
+    // if (availableMoves.length === 9) {
+    //   aiMoveEasy();
+    // } else {
+    //   aiMove();
+    // }
   };
 
   const render = (box, id) => {
     // render player move
     playerMove(box, id);
 
-    if (hasWon() || availableMoves.length === 0) {
+    if (hasWon() !== null) {
       isOver = true;
     }
-
+    // console.log(currentPlayer);
     if (gameMode === 2 && isOver === false) {
-      setTimeout(aiMove, 400);
+      aiMoveHard();
     }
 
-    if (hasWon() || availableMoves.length === 0) {
+    if (hasWon() !== null) {
       isOver = true;
     }
+    // console.log(currentPlayer);
   };
 
   const getIsOver = () => isOver;
